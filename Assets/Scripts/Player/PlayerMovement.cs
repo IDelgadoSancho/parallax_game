dc67e4859150549cb9084373c1241d0b8cab6881
Ismael <ismaeldelgado117@gmail.com>
2026-06-19 15:52:10 +0200

    player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 8f;
+
+    private Rigidbody2D rb;
+    private InputSystem_Actions inputActions;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputActions = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.Player.Move.performed += OnMove;
+        inputActions.Player.Move.canceled += OnMove;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Player.Move.performed -= OnMove;
+        inputActions.Player.Move.canceled -= OnMove;
+        inputActions.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = Vector2.Lerp(rb.linearVelocity, moveInput * speed, 0.15f);
+        transform.rotation = Quaternion.Euler(0, 0, -moveInput.x * 10f);
+    }
+
+}
